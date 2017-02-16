08b69f0b8cd92c12f9c90ea52622bffbc0943e6e
Maria López <marialopezvivas@gmail.com>
2017-02-16 11:30:54 +0100

    Puntos 1-3

---
+++ RandomEnemyGenerator/Header.h
@@ -0,0 +1,5 @@
+#pragma once
+#include <iostream>
+using namespace std;
+
+enum EnemyType { zombie, vampire, ghost, witch };
---
+++ RandomEnemyGenerator/RandomEnemyGenerator.cpp
@@ -0,0 +1,41 @@
+#include "Header.h"
+#include <time.h>
+#include <math.h>
+#include <iostream>
+using namespace std;
+
+string names[]{ "Pinchependejo", "Antxel", "Shrek", "Merienda" };
+
+enum EnemyType {zombie, vampire, ghost, witch};
+
+struct Enemy
+{
+	EnemyType type;
+	string name;
+	int health;
+};
+
+bool operator == (Enemy A, Enemy B)
+{
+	if (A.name == B.name)
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+	if (A.type == B.type)
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+};
+
+Enemy CreateRandomEnemy()
+{
+	
+}
